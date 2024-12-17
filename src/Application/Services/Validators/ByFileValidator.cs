c7eb8ff0833e4924214bb994769d95548f04be69
matteus henryk <matteus.henryk2@gmail.com>
2024-12-16 23:12:48 -0300

    feat: request validators

---
+++ src/Application/Services/Validators/ByFileValidator.cs
@@ -0,0 +1,39 @@
+﻿using CDNConverter.API.Shared.Exceptions;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CDNConverter.API.Application.Services.Validators
+{
+    public class ByFileValidator : AbstractValidator<IFormFile>
+    {
+        public ByFileValidator()
+        {
+            RuleFor(log => log.Length).NotEmpty().WithMessage(ResourceMessagesExceptions.CONTENT_EMPYT);
+            RuleFor(log => log).Must(ValidateOriginalLogFormat).WithMessage(ResourceMessagesExceptions.FORMAT_INVALID);
+        }
+
+        private bool ValidateOriginalLogFormat(IFormFile file)
+        {
+            var logPattern = @"^\d{3}\|\d{3}\|[A-Za-z0-9]+?\|\""[A-Za-z]+ \/[^"" ]+ HTTP\/\d+\.\d+\""\|\d+\.\d+$";
+            var isValid = false;
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (Regex.IsMatch(line, logPattern))
+                        isValid = true;
+                    else
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            return isValid;
+        }
+    }
+}
---
+++ src/Application/Services/Validators/ByIdValidator.cs
@@ -0,0 +1,23 @@
+﻿using CDNConverter.API.Shared.Exceptions;
+using FluentValidation;
+using System;
+
+namespace CDNConverter.API.Application.Services.Validators
+{
+    public class ByIdValidator : AbstractValidator<Guid>
+    {
+        public ByIdValidator()
+        {
+            RuleFor(id => id).NotEmpty().WithMessage(ResourceMessagesExceptions.EMPYT_IDENTIFIER);
+            RuleFor(id => id).Must(ValidateGuid).WithMessage(ResourceMessagesExceptions.EMPYT_IDENTIFIER);
+        }
+
+        private bool ValidateGuid(Guid id)
+        {
+            var isValid = true;
+            if (id == Guid.Empty) isValid = false;
+            
+            return isValid;
+        }
+    }
+}
